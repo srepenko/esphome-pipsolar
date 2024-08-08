a75c4841376319cbb49d9026325bc25d3d3429c3
Sergey Repenko <srepenko@gmail.com>
2024-08-08 16:20:31 +0300

    modified:   components/pipsolar/sensor/__init__.py

--- components/pipsolar/sensor/__init__.py
+++ components/pipsolar/sensor/__init__.py
@@ -73,6 +73,24 @@ CONF_EEPROM_VERSION = "eeprom_version"
 CONF_DEVICE_PROTOCOL_ID = "device_protocol_id"
 CONF_PV_CHARGING_POWER = "pv_charging_power"
 
+# QET<CRC><cr>: Query total PV generated energy 
+CONF_TOTAL_PV_GENERATED_ENERGY = "total_pv_generated_energy"
+# QEYyyyy<CRC><cr>: Query PV generated energy of year
+CONF_YEAR_PV_GENERATED_ENERGY = "year_pv_generated_energy"
+# QEMyyyymm<CRC><cr>: Query PV generated energy of month 
+CONF_MONTH_PV_GENERATED_ENERGY = "month_pv_generated_energy"
+# QEDyyyymmdd<CRC><cr>: Query PV generated energy of day 
+CONF_DAY_PV_GENERATED_ENERGY = "day_pv_generated_energy"
+# QLT<CRC><cr>: Query total output load energy
+CONF_DAY_PV_GENERATED_ENERGY = "total_output_load_energy"
+# QLYyyyy<CRC><cr>: Query output load energy of year 
+CONF_DAY_PV_GENERATED_ENERGY = "year_output_load_energy"
+# QLMyyyymm<CRC><cr>: Query output load energy of month 
+CONF_DAY_PV_GENERATED_ENERGY = "month_output_load_energy"
+# QLDyyyymmdd<CRC><cr>: Query output load energy of day 
+CONF_DAY_PV_GENERATED_ENERGY = "day_output_load_energy"
+# QBMS<CRC><cr>: BMS message
+# PBMS<CRC><cr>: BMS message 
 
 
 TYPES = {
@@ -273,6 +291,54 @@ TYPES = {
         accuracy_decimals=1,
         device_class=DEVICE_CLASS_POWER,
     ),
+    CONF_TOTAL_PV_GENERATED_ENERGY: sensor.sensor_schema(
+        unit_of_measurement=UNIT_WATT,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_POWER,
+    ),
+    CONF_YEAR_PV_GENERATED_ENERGY: sensor.sensor_schema(
+        unit_of_measurement='kWh',
+        accuracy_decimals=2,
+        state_class= 'total_increasing',
+        device_class= 'energy',
+    ),
+    CONF_MONTH_PV_GENERATED_ENERGY: sensor.sensor_schema(
+        unit_of_measurement='kWh',
+        accuracy_decimals=2,
+        state_class= 'total_increasing',
+        device_class= 'energy',
+    ),
+    CONF_DAY_PV_GENERATED_ENERGY: sensor.sensor_schema(
+        unit_of_measurement='kWh',
+        accuracy_decimals=2,
+        state_class= 'total_increasing',
+        device_class= 'energy',
+    ),
+    CONF_DAY_PV_GENERATED_ENERGY: sensor.sensor_schema(
+        unit_of_measurement='kWh',
+        accuracy_decimals=2,
+        state_class= 'total_increasing',
+        device_class= 'energy',
+    ),
+    CONF_DAY_PV_GENERATED_ENERGY: sensor.sensor_schema(
+        unit_of_measurement='kWh',
+        accuracy_decimals=2,
+        state_class= 'total_increasing',
+        device_class= 'energy',
+    ),
+    CONF_DAY_PV_GENERATED_ENERGY: sensor.sensor_schema(
+        unit_of_measurement='kWh',
+        accuracy_decimals=2,
+        state_class= 'total_increasing',
+        device_class= 'energy',
+    ),
+    CONF_DAY_PV_GENERATED_ENERGY: sensor.sensor_schema(
+        unit_of_measurement='kWh',
+        accuracy_decimals=2,
+        state_class= 'total_increasing',
+        device_class= 'energy',
+    ),
+
 }
 
 CONFIG_SCHEMA = PIPSOLAR_COMPONENT_SCHEMA.extend(
